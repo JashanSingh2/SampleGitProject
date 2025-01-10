59529878ce9a1a00786730a773318c56aaf3a5a0
jashan singh <jashanjatt229@gmail.com>
2025-01-10 10:41:45 +0530

    .gitignore is now working

---
+++ SampleGitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleGitProject
+//
+//  Created by Batch - 1 on 10/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
